69a9cd5a1ca9f296f01c39386d8ea1f906f6a5c5
Salem4tech <137092623+Salem4tech@users.noreply.github.com>
2024-08-26 19:04:55 +0100

    New component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
 
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
 
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"></div>
+
+      <ButtonGradient />
+    </>
+  );
+};
 
 export default App;
