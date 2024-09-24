f1102e71b1b0f74238e458c26b89c6c4895ac417
SalemDevX <137092623+SalemDevX@users.noreply.github.com>
2024-09-24 21:10:59 +0100

    Update

---
+++ src/components/Roadmap.jsx
@@ -0,0 +1,13 @@
+import Section from "./Section";
+
+const Roadmap = () => {
+  return (
+    <Section className="overflow-hidden" id="roadmap">
+      <div className="container md:pb-10">
+        <Heading tag="Ready to get started" title="What we’re working on" />
+      </div>
+    </Section>
+  );
+};
+
+export default Roadmap;
